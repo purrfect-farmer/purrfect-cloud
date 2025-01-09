6b4bf961c6ae98cae2b2ebbdad46de6ae8001598
Sadiq Salau <sadiqsalau888@gmail.com>
2025-01-09 06:40:25 +0100

    Lock Process

--- app/Console/Commands/FarmFunatic.php
+++ app/Console/Commands/FarmFunatic.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Helpers;
 use App\Models\Account;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Sleep;
@@ -31,105 +32,107 @@ class FarmFunatic extends Command
     public function handle()
     {
 
-        // Retrieve Accounts
-        $accounts = Account::where('farmer', 'funatic')
-            ->get()->map(function (Account $account) {
-                try {
+        Cache::lock($this->signature)->get(function () {
+            // Retrieve Accounts
+            $accounts = Account::where('farmer', 'funatic')
+                ->get()->map(function (Account $account) {
+                    try {
 
-                    /** API */
-                    $api = Http::withHeaders($account->headers)
-                        ->withUserAgent(
-                            $account->headers['User-Agent'] ?: Helpers::getUserAgent($account->user_id)
+                        /** API */
+                        $api = Http::withHeaders($account->headers)
+                            ->withUserAgent(
+                                $account->headers['User-Agent'] ?: Helpers::getUserAgent($account->user_id)
+                            );
+
+                        /** Get Boosters */
+                        $boosters = $api->get('https://clicker.api.funtico.com/boosters')->json('data');
+                        $availableBoosters = collect($boosters)->filter(
+                            fn($item) => (
+                                $item['price'] === 0 &&
+                                $item['isActive'] === false &&
+                                $item['cooldownLeft'] === 0 &&
+                                $item['usagesLeft'] !== 0
+                            )
                         );
 
-                    /** Get Boosters */
-                    $boosters = $api->get('https://clicker.api.funtico.com/boosters')->json('data');
-                    $availableBoosters = collect($boosters)->filter(
-                        fn($item) => (
-                            $item['price'] === 0 &&
-                            $item['isActive'] === false &&
-                            $item['cooldownLeft'] === 0 &&
-                            $item['usagesLeft'] !== 0
-                        )
-                    );
+                        /** Purchase Booster */
+                        if ($availableBoosters->isNotEmpty()) {
+                            $availableBoosters->each(function ($booster) use ($api) {
+                                /** Activate Booster */
+                                $api->post(
+                                    'https://clicker.api.funtico.com/boosters/activate',
+                                    [
+                                        'boosterType' => $booster['type']
+                                    ]
+                                );
+                            });
+                        }
 
-                    /** Purchase Booster */
-                    if ($availableBoosters->isNotEmpty()) {
-                        $availableBoosters->each(function ($booster) use ($api) {
-                            /** Activate Booster */
-                            $api->post(
-                                'https://clicker.api.funtico.com/boosters/activate',
+
+                        /** Get Game */
+                        $game = $api->get('https://clicker.api.funtico.com/game')->json('data');
+                        $energy = $game['energy']['currentEnergyBalance'];
+
+
+                        /** Return Energy and Account */
+                        if ($energy > 0) {
+                            return compact(
+                                'account',
+                                'energy'
+                            );
+                        }
+                    } catch (\Throwable $e) {
+                        $account->delete();
+                    }
+                })->filter();
+
+
+
+            /** Tap */
+            while ($accounts->isNotEmpty()) {
+                $accounts = $accounts->map(function ($item) {
+                    try {
+                        $account = $item['account'];
+                        $energy = $item['energy'];
+
+                        $taps = min($energy, 8 + rand(0, 2));
+                        $energy -= $taps;
+
+
+
+                        /** Tap */
+                        Http::withHeaders($account->headers)
+                            ->withUserAgent(
+                                $account->headers['User-Agent'] ?: Helpers::getUserAgent($account->user_id)
+                            )
+                            ->post(
+                                'https://clicker.api.funtico.com/tap',
                                 [
-                                    'boosterType' => $booster['type']
+                                    'taps' => $taps
                                 ]
                             );
-                        });
+
+                        /** Return Energy and Account */
+                        if ($energy > 0) {
+                            return compact(
+                                'account',
+                                'energy'
+                            );
+                        }
+                    } catch (\Throwable $e) {
                     }
+                })->filter();
 
 
-                    /** Get Game */
-                    $game = $api->get('https://clicker.api.funtico.com/game')->json('data');
-                    $energy = $game['energy']['currentEnergyBalance'];
+                /** Log Taps */
+                Log::info('Funatic Taps - ' . now());
 
+                /** Sleep */
+                Sleep::for(500)->milliseconds();
+            }
 
-                    /** Return Energy and Account */
-                    if ($energy > 0) {
-                        return compact(
-                            'account',
-                            'energy'
-                        );
-                    }
-                } catch (\Throwable $e) {
-                    $account->delete();
-                }
-            })->filter();
-
-
-
-        /** Tap */
-        while ($accounts->isNotEmpty()) {
-            $accounts = $accounts->map(function ($item) {
-                try {
-                    $account = $item['account'];
-                    $energy = $item['energy'];
-
-                    $taps = min($energy, 8 + rand(0, 2));
-                    $energy -= $taps;
-
-
-
-                    /** Tap */
-                    Http::withHeaders($account->headers)
-                        ->withUserAgent(
-                            $account->headers['User-Agent'] ?: Helpers::getUserAgent($account->user_id)
-                        )
-                        ->post(
-                            'https://clicker.api.funtico.com/tap',
-                            [
-                                'taps' => $taps
-                            ]
-                        );
-
-                    /** Return Energy and Account */
-                    if ($energy > 0) {
-                        return compact(
-                            'account',
-                            'energy'
-                        );
-                    }
-                } catch (\Throwable $e) {
-                }
-            })->filter();
-
-
-            /** Log Taps */
-            Log::info('Funatic Taps - ' . now());
-
-            /** Sleep */
-            Sleep::for(500)->milliseconds();
-        }
-
-        /** Log Farming Completion */
-        Log::info('Completed Funatic Farming - ' . now());
+            /** Log Farming Completion */
+            Log::info('Completed Funatic Farming - ' . now());
+        });
     }
 }
--- app/Console/Commands/FarmGoldEagle.php
+++ app/Console/Commands/FarmGoldEagle.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Helpers;
 use App\Models\Account;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
@@ -30,35 +31,37 @@ class FarmGoldEagle extends Command
      */
     public function handle()
     {
-        // Start Farming
-        Account::where('farmer', 'gold-eagle')
-            ->get()
-            ->each(function (Account $account) {
-                try {
-                    /** API */
-                    $api = Http::withHeaders($account->headers)
-                        ->withUserAgent(
-                            $account->headers['User-Agent'] ?: Helpers::getUserAgent($account->user_id)
-                        );
+        Cache::lock($this->signature)->get(function () {
+            // Start Farming
+            Account::where('farmer', 'gold-eagle')
+                ->get()
+                ->each(function (Account $account) {
+                    try {
+                        /** API */
+                        $api = Http::withHeaders($account->headers)
+                            ->withUserAgent(
+                                $account->headers['User-Agent'] ?: Helpers::getUserAgent($account->user_id)
+                            );
 
-                    /** Get Progress */
-                    $progress = $api->get('https://gold-eagle-api.fly.dev/user/me/progress')->json();
+                        /** Get Progress */
+                        $progress = $api->get('https://gold-eagle-api.fly.dev/user/me/progress')->json();
 
-                    /** Tap */
-                    if ($progress['energy'] >= 10) {
-                        $api->post('https://gold-eagle-api.fly.dev/tap', [
-                            'available_taps' => 1,
-                            'count' => $progress['energy'],
-                            'timestamp' => time(),
-                            'salt' => Str::uuid()->toString()
-                        ])->json();
+                        /** Tap */
+                        if ($progress['energy'] >= 10) {
+                            $api->post('https://gold-eagle-api.fly.dev/tap', [
+                                'available_taps' => 1,
+                                'count' => $progress['energy'],
+                                'timestamp' => time(),
+                                'salt' => Str::uuid()->toString()
+                            ])->json();
+                        }
+                    } catch (\Throwable $e) {
+                        $account->delete();
                     }
-                } catch (\Throwable $e) {
-                    $account->delete();
-                }
-            });
+                });
 
-        // Log Farming Completion
-        Log::info('Completed Gold Eagle Farming - ' . now());
+            // Log Farming Completion
+            Log::info('Completed Gold Eagle Farming - ' . now());
+        });
     }
 }
