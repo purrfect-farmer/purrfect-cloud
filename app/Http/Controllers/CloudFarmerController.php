98f917c99ac8c3de2f601bba4ae3bd17c92da06b
Sadiq Salau <sadiqsalau888@gmail.com>
2025-01-15 09:10:24 +0100

    Allow only members

--- app/Http/Controllers/CloudFarmerController.php
+++ app/Http/Controllers/CloudFarmerController.php
@@ -6,6 +6,7 @@ use App\Models\Account;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class CloudFarmerController extends Controller
 {
@@ -28,13 +29,39 @@ class CloudFarmerController extends Controller
 
 
         try {
-            return Account::updateOrCreate([
+            /** Get Account */
+            $account = Account::where([
                 'farmer' => $data['farmer'],
                 'user_id' => $data['user_id'],
-            ], [
-                'telegram_web_app' => $data['telegram_web_app'],
-                'headers' => $data['headers'],
-            ]);
+            ])->first();
+
+            /** Update Account */
+            if ($account) {
+                return tap($account)->update([
+                    'telegram_web_app' => $data['telegram_web_app'],
+                    'headers' => $data['headers'],
+                ]);
+            } else {
+                /** Get Member */
+                $member = Telegram::bot()->getChatMember([
+                    'chat_id' => env('TELEGRAM_CHAT_ID'),
+                    'user_id' =>  $data['user_id']
+                ]);
+
+                /** Ensure user is in chat */
+                if (
+                    collect(['creator', 'administrator', 'member'])->contains($member->status)
+                ) {
+                    return Account::create([
+                        'farmer' => $data['farmer'],
+                        'user_id' => $data['user_id'],
+                        'telegram_web_app' => $data['telegram_web_app'],
+                        'headers' => $data['headers'],
+                    ]);
+                } else {
+                    abort(400, 'Not a Member of Chat!');
+                }
+            }
         } catch (\Throwable $e) {
             abort(400, $e->getMessage());
         }
