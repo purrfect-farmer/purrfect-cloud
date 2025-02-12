b4a9a5b1563e4623b39bb2ddbcfd94ff277e59b1
Sadiq Salau <sadiqsalau888@gmail.com>
2025-02-13 00:35:41 +0100

    Added Server Route

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+/** Server */
+Route::get('server', function () {
+    return ['name' => config('app.name')];
+});
+
+/** Sync */
 Route::post('sync', [CloudFarmerController::class, 'sync']);
 
+/** Authenticated Routes */
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/accounts', [CloudFarmerController::class, 'accounts']);
     Route::post('/accounts/{account}/disconnect', [CloudFarmerController::class, 'disconnect']);
