aed5713a9b7c7e8f9146bd97bfc0129aa05e2cff
Sadiq Salau <sadiqsalau888@gmail.com>
2025-02-14 16:53:04 +0100

    Updated Gold Eagle

--- app/Console/Commands/FarmGoldEagle.php
+++ app/Console/Commands/FarmGoldEagle.php
@@ -22,7 +22,7 @@ class FarmGoldEagle extends Command
      * Index Script
      * @var string
      */
-    const INDEX_SCRIPT = 'index-DqNrnL0J';
+    const INDEX_SCRIPT = 'index-D9L4zlpE';
 
     /**
      * TOTP Secret
