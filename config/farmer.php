37ca79eaa745f1f5fbbec620de2d92abc6fe41e5
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-03 05:54:30 +0100

    Enable Farmers By Default

--- config/farmer.php
+++ config/farmer.php
@@ -1,31 +1,31 @@
 <?php
 
 return [
-    'display_farmer_title' => env('DISPLAY_FARMER_TITLE', false),
+    'display_farmer_title' => env('DISPLAY_FARMER_TITLE', true),
     'drops' => [
         'gold-eagle' => [
             'title' => '🥇 Gold Eagle Farmer',
-            'enabled' => env('ENABLE_GOLD_EAGLE_FARMER', false)
+            'enabled' => env('ENABLE_GOLD_EAGLE_FARMER', true)
         ],
         'hrum' => [
             'title' => '🥠 Hrum Farmer',
-            'enabled' => env('ENABLE_HRUM_FARMER', false)
+            'enabled' => env('ENABLE_HRUM_FARMER', true)
         ],
         'wonton' => [
             'title' => '👨‍🍳 Wonton Farmer',
-            'enabled' => env('ENABLE_WONTON_FARMER', false)
+            'enabled' => env('ENABLE_WONTON_FARMER', true)
         ],
         'funatic' => [
             'title' => '🤡 Funatic Farmer',
-            'enabled' => env('ENABLE_FUNATIC_FARMER', false)
+            'enabled' => env('ENABLE_FUNATIC_FARMER', true)
         ],
         'slotcoin' => [
             'title' => '🎰 Slotcoin Farmer',
-            'enabled' => env('ENABLE_SLOTCOIN_FARMER', false)
+            'enabled' => env('ENABLE_SLOTCOIN_FARMER', true)
         ],
         'dreamcoin' => [
             'title' => '🔋 DreamCoin Farmer',
-            'enabled' => env('ENABLE_DREAMCOIN_FARMER', false)
+            'enabled' => env('ENABLE_DREAMCOIN_FARMER', true)
         ],
     ]
 ];
