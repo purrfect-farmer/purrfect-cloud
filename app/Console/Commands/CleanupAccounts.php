bb07927c25fb81e6a8c5bc1380fe7d2fa220138b
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-04 17:56:17 +0100

    Added Command to Cleanup Accounts

---
+++ app/Console/Commands/CleanupAccounts.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Account;
+use Illuminate\Console\Command;
+
+class CleanupAccounts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cleanup:accounts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cleanup Accounts';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Account::whereNotIn(
+            'farmer',
+            collect(config('farmer.drops'))
+                ->filter(fn($drop) => $drop['enabled'])
+                ->keys()
+        )->delete();
+    }
+}
