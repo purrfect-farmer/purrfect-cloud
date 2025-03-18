36115c597e02e71a71e5c6e96405c6f3b5b0b626
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-18 08:08:36 +0100

    Added Telegram Logout

--- app/Console/Commands/TelegramLogout.php
+++ app/Console/Commands/TelegramLogout.php
@@ -5,21 +5,21 @@ namespace App\Console\Commands;
 use App\Facades\Madeline;
 use Illuminate\Console\Command;
 
-class TelegramLogin extends Command
+class TelegramLogout extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'telegram:login';
+    protected $signature = 'telegram:logout';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Telegram Login';
+    protected $description = 'Telegram Logout';
 
     /**
      * Execute the console command.
@@ -27,6 +27,6 @@ class TelegramLogin extends Command
     public function handle()
     {
         $client = Madeline::session();
-        $client->start();
+        $client->logout();
     }
 }
