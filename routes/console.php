37f1f37a56410868a05f42cc06dfcfd325730a4e
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-30 21:20:51 +0100

    Set Cache Limit to 60

--- routes/console.php
+++ routes/console.php
@@ -21,7 +21,7 @@ collect(config('farmer.drops'))
     ->keys()
     ->each(
         fn($key) => Schedule::command('farm:' . $key)
-            ->withoutOverlapping()
+            ->withoutOverlapping(60)
             ->onOneServer()
             ->runInBackground()
             ->everyTenMinutes()
