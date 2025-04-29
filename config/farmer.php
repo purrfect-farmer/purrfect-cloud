7f13c5e11a54604dcec9677b9e7d6f9a1804cc3f
Sadiq Salau <sadiqsalau888@gmail.com>
2025-04-29 08:07:44 +0100

    Run in background

--- config/farmer.php
+++ config/farmer.php
@@ -24,7 +24,7 @@ return [
     'enable_payments' => env('FARMER_ENABLE_PAYMENTS', true),
     'subscription_amount' => env('FARMER_SUBSCRIPTION_AMOUNT', 1550),
 
-    'run_in_background' => env('FARMER_RUN_IN_BACKGROUND', false),
+    'run_in_background' => env('FARMER_RUN_IN_BACKGROUND', true),
 
     'proxy' => [
         'enabled' => env('FARMER_PROXY_ENABLED', false),
